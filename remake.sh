8516aa341b1ffd266f4cd4684bb3791c3f6f98c8
Andrii Klubuk <andrii.klubuk@shelf.io>
2021-07-19 15:30:20 +0300

    fix cat

--- remake.sh
+++ remake.sh
@@ -13,18 +13,18 @@ chmod +x ./.husky/post-commit
 rm ./.husky/pre-commit || true
 touch ./.husky/pre-commit
 chmod +x ./.husky/pre-commit
-cat <<EOT >> ./.husky/pre-commit
+cat <<EOF >> ./.husky/pre-commit
 #!/bin/sh
 . "$(dirname "$0")/_/husky.sh"
 
 yarn lint-staged
 yarn type-check
-EOT
-cat <<EOT >> ./.husky/post-commit
+EOF
+cat <<EOF >> ./.husky/post-commit
 #!/bin/sh
 . "$(dirname "$0")/_/husky.sh"
 
 git update-index --again
-EOT
+EOF
 brew install jq || sudo apt-get install jq || true
 (jq 'del(.husky)' package.json > package.json.tmp && rm package.json && mv package.json.tmp package.json) || echo "Please manually remove entry about husky from package.json"
