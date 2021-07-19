7fcf9b2332a1770e07eb4a7003a4ea07c8868ef9
Andrii Klubuk <andrii.klubuk@shelf.io>
2021-07-19 14:27:44 +0300

    Do not remowe prepare step

--- remake.sh
+++ remake.sh
@@ -21,7 +21,5 @@ cat <<EOT >> ./.husky/post-commit
 
 git update-index --again
 EOT
-sed '/\"husky\"/,/}/ d; /^$/d' package.json
 brew install jq || sudo apt-get install jq || true
 (jq 'del(.husky)' package.json > package.json.tmp && rm package.json && mv package.json.tmp package.json) || echo "Please manually remove entry about husky from package.json"
-(jq 'del(.scripts.prepare)' package.json > package.json.tmp && rm package.json && mv package.json.tmp package.json) || true
