108b37a4117f85dc677a4fe06fbce6c90f0cbb5f
Andrii Klubuk <andrii.klubuk@shelf.io>
2021-07-19 14:04:27 +0300

    rework remake command

--- remake.sh
+++ remake.sh
@@ -7,5 +7,16 @@ chmod +x ./.husky/post-commit
 rm ./.husky/pre-commit || true
 touch ./.husky/pre-commit
 chmod +x ./.husky/pre-commit
-cat ./pre-commit > ./.husky/pre-commit
-cat ./post-commit > ./.husky/post-commit
+cat <<EOT >> ./.husky/pre-commit
+#!/bin/sh
+. "$(dirname "$0")/_/husky.sh"
+
+yarn lint-staged
+yarn type-check
+EOT
+cat <<EOT >> ./.husky/post-commit
+#!/bin/sh
+. "$(dirname "$0")/_/husky.sh"
+
+git update-index --again
+EOT
