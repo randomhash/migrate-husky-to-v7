8fdc28c801ca2cad8991cda1c813616dcb6d8c77
Andrii Klubuk <andrii.klubuk@shelf.io>
2021-07-19 13:41:18 +0300

    init

---
+++ remake.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+yarn add husky@6 --dev && npx husky-init && npm exec -- github:typicode/husky-4-to-6 --remove-v4-config
+echo 'Config recreated'
+touch ./.husky/post-commit
+chmod +x ./.husky/post-commit
+rm ./.husky/pre-commit || true
+touch ./.husky/pre-commit
+chmod +x ./.husky/pre-commit
+cat ./pre-commit > ./.husky/pre-commit
+cat ./post-commit > ./.husky/post-commit
